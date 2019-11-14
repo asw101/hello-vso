717f511776995ec7056a50b1f8a16848e9faa7cd
Aaron Wislang <Aaron.Wislang@microsoft.com>
2019-11-14 03:19:11 +0000

    run 0-all.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,3 @@
 echo "hello world"
-#cp .devcontainer/keybindings.json /User/keybindings.json
-#cp .devcontainer/settings.json /User/settings.json
 cd .devcontainer/
-source 2-golang.sh
-source 3-misc.sh
-source 4-rustlang.sh
+source 0-all.sh
